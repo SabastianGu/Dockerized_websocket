2d569a3d12e69c1baeb777f816e1113bffad8c99
Saba <102615105+SabastianGu@users.noreply.github.com>
2023-03-24 04:44:54 +0400

    Update consumers.py

--- test_project/WebService/engine_side/consumers.py
+++ test_project/WebService/engine_side/consumers.py
@@ -16,28 +16,62 @@ import json
 import math
 
 class TheConsumer(AsyncWebsocketConsumer):
+    CALCULATE_ACTION = 'calculate'
+    PERFECT_ACTION = 'Check if number is perfect'
     async def connect(self):
-        await self.accept()
+        try:
+            await self.accept()
+        except Exception as e:
+            print(f"Failed to connect to WebSocket: {e}")
 
     async def disconnect(self, close_code):
-        pass
+        try:
+            await self.close()
+        except Exception as e:
+            print(f"Failed to disconnect from WebSocket: {e}")
 
     async def receive(self, text_data):
-        data = json.loads(text_data)
-        action = data['action']
-        value = data['value']
+        try:
+            data = json.loads(text_data)
+            action = data.get('action')
+            value = data.get('value')
 
-        if action == 'calculate':
-            try:
-                number = int(value)
-                result = math.factorial(number)
-                response = {'result': result}
-            except ValueError:
-                response = {'error': 'Invalid input'}
-        else:
-            response = {'error': 'Invalid action'}
+            if action == self.CALCULATE_ACTION:
+                try:
+                    number = int(value)
+                    result = math.factorial(number)
+                    response = {'result': result}
+                except ValueError:
+                    response = {'error': 'Invalid input'}
+            elif action == self.PERFECT_ACTION:
+                try:
+                    number = int(value)
+                    result, explanation = self.calculate_perfect_number(number)
+                    response = {'result': result, 'explanation': explanation}
+                except ValueError:
+                    response = {'error': 'Invalid input'}
+            else:
+                response = {'error': 'Invalid action'}
 
-        await self.send(json.dumps(response))
+            await self.send(json.dumps(response))
+        except Exception as e:
+            print(f"Failed to handle WebSocket message: {e}")
+
+    def calculate_perfect_number(self, number):
+        if number <= 0:
+            raise ValueError("Number must be positive")
+
+        factors = [i for i in range(1, number) if number % i == 0]
+        if sum(factors) == number:
+            return number, f"{number} is a perfect number"
+
+        i = 1
+        while True:
+            candidate = number + i
+            factors = [j for j in range(1, candidate) if candidate % j == 0]
+            if sum(factors) == candidate:
+                return candidate, f"{number} is not a perfect number, but the nearest perfect number is {candidate}"
+            i += 1
 
 class UserConsumer(
         mixins.ListModelMixin,
