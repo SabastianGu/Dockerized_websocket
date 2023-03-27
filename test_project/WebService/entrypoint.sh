0b64fe196bc89720245baa3b12f8d05657343df7
Saba <102615105+SabastianGu@users.noreply.github.com>
2023-03-27 18:11:14 +0400

    Update entrypoint.sh

--- test_project/WebService/entrypoint.sh
+++ test_project/WebService/entrypoint.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
-cd /app/WebService
-python3 manage.py makemigrations --no-input
-python3 manage.py migrate --no-input
-python3 manage.py collectstatic --no-input
 
+python3 manage.py makemigrations &
+python3 manage.py migrate &
+python3 manage.py collectstatic --no-input &
 gunicorn -b 0.0.0.0:8000 ws_test.wsgi:application & 
 daphne -b 0.0.0.0 -p 9000 ws_test.asgi:application
